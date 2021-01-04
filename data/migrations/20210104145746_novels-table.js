e3cb99dc469dfabd91a20a3e6bb9d0c85c3339d8
Keenan <keenanhallas@gmail.com>
2021-01-04 15:44:05 -0600

    adds novels table to DB schema

---
+++ data/migrations/20210104145746_novels-table.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('novels', tbl => {
+    tbl.increments('id')
+        .notNullable()
+        .unique()
+        .primary()
+        .unsigned();
+    tbl.string('title')
+        .notNullable()
+        .unique();
+    tbl.string('description')
+        .notNullable();
+    tbl.integer('chapters')
+        .notNullable();
+    tbl.integer('pages')
+        .notNullable()
+        .unsigned();
+  });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('novels');
+};

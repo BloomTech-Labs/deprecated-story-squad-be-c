4ba6f87481995d7dc9f5fbdc6a174d6cfb3023cd
Keenan <keenanhallas@gmail.com>
2021-01-04 16:13:24 -0600

    alters code formatting further to fix prettier errors, round 3

--- data/migrations/20210104145746_novels-table.js
+++ data/migrations/20210104145746_novels-table.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('novels', (tbl) => {
     tbl.increments('id').notNullable().unique().primary().unsigned();
     tbl.string('title').notNullable().unique();
@@ -8,6 +8,6 @@ exports.up = function(knex) {
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTableIfExists('novels');
 };

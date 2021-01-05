48e48f48d55fe5dae4ab300bb8da8fab0e77969b
Keenan <keenanhallas@gmail.com>
2021-01-05 14:55:22 -0600

    completes the exports.up & exports.down functions for creating an EmojiFeedback column in the WRITING table

--- data/migrations/20210105143912_modifies-writing-table.js
+++ data/migrations/20210105143912_modifies-writing-table.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.table('Writing', (tbl) => {
+      tbl.string('EmojiFeedback');
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function (knex) {
+    return knex.schema.table('Writing', (tbl) => {
+      tbl.dropColumn('EmojiFeedback');
+    });
+  };
   
-};

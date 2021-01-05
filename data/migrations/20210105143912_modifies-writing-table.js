359551096501126b9c3c5108df665157de1fdd41
Keenan <keenanhallas@gmail.com>
2021-01-05 15:07:25 -0600

    fixes formatting to match Prettier standards

--- data/migrations/20210105143912_modifies-writing-table.js
+++ data/migrations/20210105143912_modifies-writing-table.js
@@ -1,12 +1,11 @@
 exports.up = function (knex) {
-    return knex.schema.table('Writing', (tbl) => {
-      tbl.string('EmojiFeedback');
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.table('Writing', (tbl) => {
-      tbl.dropColumn('EmojiFeedback');
-    });
-  };
-  
+  return knex.schema.table('Writing', (tbl) => {
+    tbl.string('EmojiFeedback');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table('Writing', (tbl) => {
+    tbl.dropColumn('EmojiFeedback');
+  });
+};

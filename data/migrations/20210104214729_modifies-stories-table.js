5120893e60b73764b86ef16ad64e7d74c6a04419
Keenan <keenanhallas@gmail.com>
2021-01-05 11:46:36 -0600

    adds NovelID & Chapter columns to the Stories table

--- data/migrations/20210104214729_modifies-stories-table.js
+++ data/migrations/20210104214729_modifies-stories-table.js
@@ -1,9 +1,13 @@
 exports.up = function(knex) {
   return knex.schema.table('Stories', (tbl) => {
-    tbl.integer('NovelID').notNullable().references("id")
+    tbl.integer('NovelID').notNullable().references('Novels.ID').onUpdate('CASCADE').onDelete('RESTRICT');
+    tbl.integer('Chapter').unsigned().notNullable();
   });
 };
   
 exports.down = function(knex) {
-    
+  return knex.schema.table('Stories', (tbl) => {
+    tbl.dropColum('NovelID');
+    tbl.dropColum('Chapter');
+  })
 };

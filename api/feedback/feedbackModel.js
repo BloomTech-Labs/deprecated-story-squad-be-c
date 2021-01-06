ba706c47f8299d29edd84e229c88902a7f307450
Keenan <keenanhallas@gmail.com>
2021-01-06 11:54:43 -0600

    modifies code to meet Prettier standards

--- api/feedback/feedbackModel.js
+++ api/feedback/feedbackModel.js
@@ -1,5 +1,6 @@
-const db = require('../../data/db-config');
-const { ops } = require('../../lib');
+//const db = require('../../data/db-config');
+//const { ops } = require('../../lib');
+//These will be needed, but they're commented out so the linter doesn't complain
 
 //DB access functions go here and return promises. Description goes above each function.
 //See an existing model file for examples
--- api/feedback/feedbackRouter.js
+++ api/feedback/feedbackRouter.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { ops } = require('../../lib');
+//const { ops } = require('../../lib');
 const { authRequired } = require('../middleware');
 
 //Not using this yet, will be used for DB access
@@ -16,7 +16,7 @@ router.get('/', authRequired, (req, res) => {
 //This endpoint will be used to post emoji feedback for a submission
 //May need a submission id in the route, depending how you set it up
 router.post('/', authRequired, (req, res) => {
-  res.status(200).json({ endpoint: "Post emoji feedback" });
+  res.status(200).json({ endpoint: 'Post emoji feedback' });
 
   //An ops function will be used for posting to the backend, ex:
   //ops.postMult();

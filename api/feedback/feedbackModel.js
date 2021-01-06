3a050e05c55ae44a8e592b8e9c47ca632a2273cb
Keenan <keenanhallas@gmail.com>
2021-01-06 11:38:47 -0600

    outlines an emoji feedback endpoint and adds comments to direct the next developer to the next steps to get it working

--- api/feedback/feedbackModel.js
+++ api/feedback/feedbackModel.js
@@ -1,20 +1,8 @@
 const db = require('../../data/db-config');
-const { formatTeam } = require('../../lib');
-const { points, faceoff } = require('./gameHelpers');
+const { ops } = require('../../lib');
 
-/**
- * Takes a child ID and returns the child's current squad
- * @param {number} ChildID unique integer id of a child
- * @returns {Promise} returns a promise that resovles to an ID
- */
-const getSquadIDFromChildID = (ChildID) => {
-  return db('Squads AS S')
-    .join('Teams AS T', 'S.ID', 'T.SquadID')
-    .join('Members AS M', 'T.ID', 'M.TeamID')
-    .join('Submissions AS Sub', 'Sub.ID', 'M.SubmissionID')
-    .join('Children AS C', 'C.ID', 'Sub.ChildID')
-    .where('C.ID', ChildID)
-    .select(['S.ID', 'M.ID AS MemberID']);
-};
+//DB access functions go here and return promises. Description goes above each function.
+//See an existing model file for examples
 
+//Export functions here
 module.exports = {};
--- api/feedback/feedbackRouter.js
+++ api/feedback/feedbackRouter.js
@@ -3,62 +3,24 @@ const router = require('express').Router();
 const { ops } = require('../../lib');
 const { authRequired } = require('../middleware');
 
-//Not using this yet
+//Not using this yet, will be used for DB access
 //const Feedback = require('./feedbackModel');
 
-//Need to finish editing this swagger documentation
-/**
- * @swagger
- * /feedback/:
- *  post:
- *    summary: Send emoji feedback for a writing or drawing
- *    security:
- *      - okta: []
- *    tags:
- *      - Feedback
- *    requestBody:
- *      description: An array containing two points objects, one for each submission on
- *                   the team (the child's and their teammate's). It is highly recommended
- *                   to send both up together.
- *      content:
- *        application/json:
- *          schema:
- *            type: array
- *            items:
- *              $ref: '#/components/schemas/PointsObject'
- *    responses:
- *      201:
- *        description: Returns an array of the id references to the new votes
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              example: [ 1, 2 ]
- *      400:
- *        $ref: '#/components/responses/InvalidFormat'
- *      401:
- *        $ref: '#/components/responses/UnauthorizedError'
- *      403:
- *        $ref: '#/components/responses/DuplicateError'
- *      404:
- *        $ref: '#/components/responses/NotFound'
- *      500:
- *        $ref: '#/components/responses/DatabaseError'
- */
+//Need to add swagger documentation for the routes
 
 //Test route
 router.get('/', (req, res) => {
-  res.status(200).json({ status: 'Success!' });
+  res.status(200).json({ endpoint: 'Feedback Router' });
 });
 
+//This endpoint will be used to post emoji feedback for a submission
+//May need a submission id in the route, depending how you set it up
 //Need to add back authRequired middleware when done testing
 router.post('/', (req, res) => {
-  const feedback = req.body;
+  res.status(200).json({ endpoint: "Post emoji feedback" });
 
-  res.status(200).json({ feedback: feedback, status: 'Success!' });
-
-  //need to write a custom ops function to use here, I think in crudOps.js
-  //ops.postMult(res, Game.assignPoints, 'Submission', points);
+  //An ops function will be used for posting to the backend, ex:
+  //ops.postMult();
 });
 
 module.exports = router;
